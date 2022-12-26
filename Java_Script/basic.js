90592a77a063c5ae98f7801bc6d5eab9a0f2ffad
Naman Parashar <namanparashar484@gmail.com>
2022-12-26 19:05:33 +0530

    JS

---
+++ Java_Script/basic.js
@@ -0,0 +1,38 @@
+var i , sum =0;
+for(i=0; i<5;i++){
+    sum += i;
+}
+document.write(sum+"<br/>")
+document.write("This script tag is in External File <br/>")
+document.write("This script tag is in External File <br/>")
+
+document.write("<b/>This is a variable whose value is not assigned<br/>")
+var s;
+document.write(s+"<br/>")
+
+s=1;
+for(; ; ){
+    document.write((s++) +"<br/>")
+    if(s==11) break
+}
+
+
+alert("Alert box")
+
+var b = confirm("Do you want this")
+if(b){
+    document.write("You Press ok <br/>")
+}
+else{
+    document.write("You Press cancel<br/>")
+}
+
+// var n = prompt("Enter your Name<br/>")
+// if(n != null && n.length > 0){
+//     document.write(n+"<br/>")
+// }
+
+document.write(eval("2+5+5"))
+document.write(parseInt("15VRV"))
+
+// ................................................................
