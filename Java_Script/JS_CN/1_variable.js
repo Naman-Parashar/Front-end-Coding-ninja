095cc548a51ea8c571435b66cd2f14738ef8d9a6
Naman Parashar <namanparashar484@gmail.com>
2023-01-09 21:38:05 +0530

    JS

---
+++ Java_Script/JS_CN/1_variable.js
@@ -0,0 +1,28 @@
+// var a = "naman";
+// document.write(a);
+
+//  a = 10;
+// document.write(a);
+
+//  a = 12.5;
+// document.write(a);
+// //  var can store different values. 
+
+// console.log(a);   // to print the value in console.
+
+// var a = "A";
+// console.log(65 - a);
+
+// var i=0 ;     
+//    while(i<5); { 
+//       console.log("hii"); 
+//       ++i ; 
+//    }ile(i<5);
+var a = 10;
+console.log(typeof(a));
+// if( typeof(a) ) {
+//     console.log("true")
+// }
+// else {
+//     console.log("false")
+// }
